0760577ed7c0d85ab84cca78855ab681a4371dbd
Jared Forsyth <jared@jaredforsyth.com>
2013-10-15 19:19:24 -0600

    heroku deployment ftw

--- worker.js
+++ worker.js
@@ -1,8 +1,22 @@
 
+var git = require('strider-git/lib')
+
 module.exports = {
-  init: function (config, job, context, cb) {
+  init: function (userConfig, config, job, context, cb) {
+    var account
+    for (var i=0; i<userConfig.accounts.length; i++) {
+      if (userConfig.accounts[i].id === config.app.account) {
+        account = userConfig.accounts[i]
+        break;
+      }
+    }
+    console.log(account)
+    if (!account) return cb(new Error('Heroku invalid configuration. Account not found'))
     cb(null, {
-      deploy: 'echo "heroku deploy"'
+      deploy: function (context, done) {
+        var cmd = 'git push ' + config.app.git_url + ' ' + context.branch + ':master'
+        git.gitaneCmd(cmd, context.dataDir, account.privkey, context, done)
+      }
     })
   }
 }
